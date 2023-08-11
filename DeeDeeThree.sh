5610d01137506ed7ede5a9367b4ad66b91886a7e
Jason D Limberis <8179171+SemiQuant@users.noreply.github.com>
2023-08-10 18:00:45 -0700

    Update DeeDeeThree.sh
    commenting

--- DeeDeeThree.sh
+++ DeeDeeThree.sh
@@ -8,13 +8,18 @@ threads="${3-6}"
 # Name file
 nm="${R1/_R1_001.fastq.gz/}"
 
+# static
+ref="path to reference fasta"
+
 # Create a reference index for alignment using bwa
-bwa index 'name of reference'
+# since using only you own, this will already be done, but you can try make it look for the reference and then make it if it doesnt exist if you want. Otherwise it would remake it each time and overwrite files etc. (human genome take hours to index)
+#bwa index ${ref}
+
 
 # Alignment pipeline
-fastqc -t $threads "$R1" |  # Quality control analysis on R1
-    tee "${nm}_R1_fastqc.log" |  # Save FastQC log
-    fastqc -t $threads "$R2" -o "${nm}_R2_fastqc"  # Quality control analysis on R2
+fastqc -t $threads "$R1" -o "${nm}_R1_fastqc"  # Quality control analysis on R1
+# tee "${nm}_R1_fastqc.log" |  # Save FastQC log
+fastqc -t $threads "$R2" -o "${nm}_R2_fastqc"  # Quality control analysis on R2
 
 # Align R1 and R2 to the reference using bwa mem and save the output as a SAM file
 bwa mem -t $threads 'name of reference' "$R1" "$R2" > "${nm}.sam"
